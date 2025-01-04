834d518e17cd49cbe361ed9e90fb791723d40c15
Josie Bealle <josiebealle@gmail.com>
2025-01-04 17:02:15 +0100

    Bug - add ingredient does not refresh data - invalidate correct query

--- web-dev/src/Hooks/Mutation/IngredientsMutation.ts
+++ web-dev/src/Hooks/Mutation/IngredientsMutation.ts
@@ -19,9 +19,33 @@ export const useMutationIngredientCreate = (): UseMutationResult<
     },
     {
       onSuccess: () => {
-        clientQuery.invalidateQueries(Requests.listRecipe);
+        clientQuery.invalidateQueries(Requests.listIngredient);
       },
     }
+  );
+};
+
+export const useMutationIngredientUpdate = (): UseMutationResult<
+    any,
+    unknown,
+    { name: string; price: number, tag: Tag, id: number }
+> => {
+  const clientQuery = useQueryClient();
+
+  return useMutation(
+      [Requests.updateIngredient],
+      async ({ name, price, tag, id }: { name: string; price: number, tag: Tag, id: number }) => {
+        return await axios.put(`/ingredient/update/${id.toString()}`, {
+          name,
+          price,
+          tag,
+        });
+      },
+      {
+        onSuccess: () => {
+          clientQuery.invalidateQueries(Requests.listIngredient);
+        },
+      }
   );
 };
 
